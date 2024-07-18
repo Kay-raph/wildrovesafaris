c2e27ec28bb871364e3fd53ab4fd444860adb97c
kay-rahp <kayraph3d@gmail.com>
2024-07-18 12:51:02 +0300

    renamed pages

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function () {
+$(".ad span").hide();
+$(".ad").click(function () {
+$(".ad span").toggle();
+$(".ad img").toggle();
+});
+
+$(".cu span").hide();
+$(".cu").click(function () {
+$(".cu span").toggle();
+$(".cu img").toggle();
+});
+
+$(".lu span").hide();
+$(".lu").click(function () {
+$(".lu span").toggle();
+$(".lu img").toggle();
+});
+
+$(".fam span").hide();
+$(".fam").click(function () {
+$(".fam span").toggle();
+$(".fam img").toggle();
+});
+
+$(".cus span").hide();
+$(".cus").click(function () {
+$(".cus span").toggle();
+$(".cus img").toggle();
+});
+
+$(".gr span").hide();
+$(".gr").click(function () {
+$(".gr span").toggle();
+$(".gr img").toggle();
+});
+
+$(".co span").hide();
+$(".co").click(function () {
+$(".co span").toggle();
+$(".co img").toggle();
+});
+});
+
+
+
